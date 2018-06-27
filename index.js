ef5788bcf3d156a7a8fc332995e3d16c1277d59b
anil <anil@foradian.com>
2018-06-27 14:35:27 +0530

    added token generation code

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var express = require("express");
+express = require("express");
 firebase = require("firebase");
-var path = require("path");
-var url = require('url');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var auth = require('./src/auth');
+ path = require("path");
+ url = require('url');
+ bodyParser = require('body-parser');
+ cors = require('cors');
+crypto = require('crypto')
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyDsId1IO77YJD3zpgik0pYpVx5RudtA6fY",
@@ -16,28 +16,46 @@ var config = {
 };
 firebase.initializeApp(config);
 var app = express();
+var messages = [];
 // var urlencodedParser = bodyParser.urlencoded({ extended: false });
 // app.use(urlencodedParser);
 app.use(bodyParser.json());
 app.use(cors());
+app.use(function(req,res,next){
+  console.log(req.headers);
+  next(undefined);
+})
+// const oAuth2Server = require('node-oauth2-server');
+// const oAuthModel = require('./src/accessTokenModel');
+// app.oauth = oAuth2Server({
+//     model: oAuthModel,
+//     grants: ['password'],
+//     debug: true
+// })
+var auth = require('./src/auth')(app);
+app.use('/auth', auth);
+// app.use(app.oauth.errorHandler());
+app.get("/users",function(req, res) {
+  firebase.database().ref("users").once("value").then(sendResponse)
+    .catch((err) => {
+      log("error occured");
+    });
 
-
-app.post("/signup", auth.signUp);
-
-app.post("/login", auth.login);
-
-app.get("/contacts", function(req, res){
-  firebase.database().ref("users").once("value").then(sendResponse);
   function sendResponse(snapshot) {
     res.write(JSON.stringify(snapshot.val()), function(err) {
       if (err) {
         console.log("error sending data");
       }
     });
-    res.end();
+    res.end("");
   }
 })
 
+app.post("/messages",function(req, res) {
+  console.log(req.body);
+  messages.push(req.body)
+  res.end("message recieved!");
+})
 app.listen(8081, function() {
   console.log("listening on port 8081...");
 });
--- src/auth.js
+++ src/auth.js
@@ -1,25 +1,33 @@
-var signUp = async function(req, res) {
+var auth = express.Router();
 
-  await firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password)
-    .then((user) => {
-      // console.log(JSON.stringify(user));
-      if (user) {
-        user = firebase.auth().currentUser;
-        user.updateProfile({
-          displayName: req.body.username,
-          photoURL: "fksdjfsd.jpg"
-        });
-        firebase.database().ref("/users/" + user.uid).set({
-          username: req.body.username,
-          email: user.email,
-        })
-      }
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorMessage);
-    })
+var signUp = async function(req, res) {
+  console.log(req.body);
+  try {
+    await firebase.auth().createUserWithEmailAndPassword("agr@gmail.com", "123456")
+      .then(async (user) => {
+        // console.log(JSON.stringify(user));
+        if (user) {
+          user = await firebase.auth().currentUser;
+          user.updateProfile({
+            displayName: req.body.username,
+            photoURL: "fksdjfsd.jpg"
+          });
+          firebase.database().ref("/users/" + user.uid).set({
+            username: "gv",
+            email: user.email,
+          })
+        }
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+      })
+  } catch (e) {
+    console.log(e.message);
+  } finally {
+
+  }
 
   res.end("successfuly signed up!");
 
@@ -27,26 +35,88 @@ var signUp = async function(req, res) {
 
 var login = async function(req, res) {
 
-  await firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password)
-    .then((user) => {
-      console.log(JSON.stringify(user));
-      if (user) {
-        user = firebase.auth().currentUser;
-        user.updateProfile({
-          displayName: req.body.username,
-          photoURL: "fksdjfsd.jpg"
-        });
-      }
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorMessage);
-    })
+  let uid;
+  try {
+    await firebase.auth().signInWithEmailAndPassword("agr@gmail.com", "123456")
+      .then(async (user) => {
+        if (user) {
+          user = await firebase.auth().currentUser;
+          uid = user.uid;
+          await user.updateProfile({
+            displayName: "req.body.username",
+            photoURL: "fksdjfsd.jpg"
+          });
+          generateAndSendAccessToken(uid, res);
+        }
 
-  res.end("successfuly logged in!");
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+      })
+
+  } catch (e) {
+    console.log(e.message);
+  } finally {
+
+  }
+  // res.end(uid);
 
 }
 
-module.exports.signUp = signUp;
-module.exports.login = login;
+function generateAndSendAccessToken(uid, res){
+  crypto.randomBytes(256,  async function (ex, buffer) {
+    if (ex){
+      console.log("error generating token");
+    }
+
+    var token = crypto
+      .createHash('sha1')
+      .update(buffer)
+      .digest('hex');
+      saveAccessToken(token, uid, res);
+
+  });
+
+}
+async function saveAccessToken(token, user_id, res){
+   await firebase.database().ref("/tokens").orderByChild("/user_id").equalTo(user_id)
+   .once('value', (snapshot)=>{
+     snapshot.forEach((child)=>{
+       child.getRef().remove()
+     })
+   })
+   await firebase.database().ref('/tokens/'+token).set({
+     user_id:user_id,
+   });
+   console.log("saved token to database:"+token);
+   sendAccessToken(user_id, res)
+
+}
+async function sendAccessToken(uid, res){
+  await firebase.database().ref("/tokens").orderByChild("/user_id").equalTo(uid)
+  .once('value', (snapshot)=>{
+    snapshot.forEach((child)=>{
+      console.log("fecthed token using uid:"+child.key);
+      res.end(JSON.stringify({
+        access_token:child.key,
+      }))
+    })
+  });
+
+}
+function getBearerToken() {
+
+}
+function checkToken(){
+
+}
+module.exports = (app) => {
+  auth.generateAccessToken = generateAndSendAccessToken;
+  auth.saveAccessToken = saveAccessToken;
+  auth.post("/signup", signUp);
+  auth.post("/login",login);
+  auth.login = login;
+  return auth;
+}
